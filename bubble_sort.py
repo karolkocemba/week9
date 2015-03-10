d7927c5ff88f2c0e452467cc9a60b6176e575e4c
karolkocemba <karol.kocemba@gmail.com>
2015-03-10 05:23:02 -0500

    bubble sort algo solved

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,16 +1,27 @@
 import random
 
 def sort(items):
-  # 1. TO DO: Implement a "bubble sort" routine here
-  pass
 
+	n = len(items)
+
+	while n > 0:
+		for i in range(n-1):
+			if items[i] > items[i+1]:
+				temp = items[i]
+				items [i] = items[i+1]
+				items [i+1] = temp
+		n-=1
+	
+	return items
 
 numbers = list(range(10))
 random.shuffle(numbers)
+# print(numbers)
+# print(sort(numbers)) 
 
 assert list(range(10)) == sort(numbers)
 print("The list was sorted correctly!")
 
 # 2. Change this print statement to display the complexity category.
 #    Refer to the cheat sheet in week9-class for examples.
-print("This algorithm is classified as: O(....)")
+print("This algorithm is classified as: O(n^2)")
